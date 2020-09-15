597a4553e04ff60cfee9f479ab346208d6171a0f
arnavpuri <arnavanytime@gmail.com>
2020-09-15 20:04:00 +0530

    Finished Game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+  let [currentWord, setCurrentWord] = useState('');
+  let [guessedWords, setGuessedWords] = useState([]);
+  let [wrongAttempts, setWrongAttempts] = useState(0);
+  let [gameOver, setGameOver] = useState(true);
+  
+  useEffect(() => {
+    setCurrentWord(getRandomWord());
+  }, []);
+
+  let words = ['hello', 'India', 'bicycle', 'universe'];
+
+  let getRandomWord = () => {
+    let randIndex = Math.floor(Math.random() * words.length)
+    return words[randIndex];
+  }
+
+  let userGuessed = (letter) => {
+    if(!guessedWords.includes(letter)){
+      setGuessedWords(guessedWords.concat(letter));
+      if(!currentWord.includes(letter)){
+        setWrongAttempts(wrongAttempts+1);
+      }
+      if(wrongAttempts > 10){
+        setGameOver(true);
+      }
+    }
+
+  }  
+  let enterPress = (e) => {
+    console.log("key pressed");
+    if(e.charCode == 13){
+      userGuessed(e.target.value);
+      e.target.value = ''
+   }
+   
+  }
+
+  let getPlaceHolder = () => {
+    let result =  Array(currentWord.length).fill(' _ ');
+    for(let i =0; i<currentWord.length; i++){
+      let letter = currentWord[i];
+      if(guessedWords.includes(letter)){
+        result[i] = ` ${letter} `;
+      }
+    }
+    return result;
+  }
+  let reset = (e) => {
+    setGameOver(false);
+    setWrongAttempts(0);
+    setGuessedWords([]);
+    setCurrentWord(getRandomWord());
+
+  }
+
+  // HTML
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <h1 className="heading">
+        Hangman Game
+        
+      </h1>
+      {!gameOver && (
+        <React.Fragment>
+          <h3 style={{color: '#B33771'}}>Wrong Attempts: {wrongAttempts}</h3>
+          <div className="hangman-container">
+            <img src={require(`./assets/${wrongAttempts}.png`)} alt=""/>
+          </div>
+          <input disabled={gameOver} type="text" maxLength="1" onKeyPress={enterPress} />
+          <div className="placeholder">{getPlaceHolder()}</div>
+        </React.Fragment>
+      )}
+      {gameOver && (
+      <React.Fragment>
+        <h2 className="red">GAME OVER!</h2>
+        <div className="hangman-container">
+          <img src={require(`./assets/11.png`)} alt=""/>
+        </div>
+        <button className="reset-btn" onClick={reset}>New Game</button>
+    </React.Fragment>
+    )}
+
     </div>
   );
 }
