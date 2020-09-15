55003191986a433165cc1e3cd5d79a174b456793
arnavpuri <arnavanytime@gmail.com>
2020-09-15 21:33:37 +0530

    Updates

---
+++ precache-manifest.80e3b4b52a74c4987607862c59319517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb6264d509106884d31d14f5a07f03c",
+    "url": "/hangman_reactjs/index.html"
+  },
+  {
+    "revision": "0c93c32b32c844359d37",
+    "url": "/hangman_reactjs/static/css/main.b6b54bf7.chunk.css"
+  },
+  {
+    "revision": "4f88f5d2aa55e648953f",
+    "url": "/hangman_reactjs/static/js/2.1300f70c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman_reactjs/static/js/2.1300f70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c93c32b32c844359d37",
+    "url": "/hangman_reactjs/static/js/main.2b9f1063.chunk.js"
+  },
+  {
+    "revision": "17592411592e27a2f369",
+    "url": "/hangman_reactjs/static/js/runtime-main.fa93b9a6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf35e1febbeda424a0653fdc6978332b.js"
+  "/hangman_reactjs/precache-manifest.80e3b4b52a74c4987607862c59319517.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hangman_reactjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
